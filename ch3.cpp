b0296ee13727db3991f67f00a97781db65fe4520
Bee <bee@macbook-pro.909.vc>
2024-05-28 09:44:42 -0400

    updates ch3

--- ch3.cpp
+++ ch3.cpp
@@ -9,7 +9,8 @@ enum measurement {
 	in,
 	cm,
 	m,
-	ft
+	ft,
+	null
 };
 
 measurement check_measurement(std::string const& mtype){
@@ -17,7 +18,7 @@ measurement check_measurement(std::string const& mtype){
 	if(mtype=="cm") return cm;
 	if(mtype=="ft") return ft;
 	if(mtype=="m") return m;
-	return m;
+	return null;
 }
 
 double convert_temp(std::string const& mtype, double value){
@@ -35,7 +36,11 @@ double convert_temp(std::string const& mtype, double value){
 		case m:
 			return_val=value;
 			break;
+		case null:
+			return_val = -1;
+			break;
 		}
+	
 return return_val;
 }
 
@@ -44,18 +49,14 @@ int main(){
 	std::string x1,y1;
 	std::vector<double> v;
 	while(std::cin>>temp>>y1>>x>>x1){
-		if(y1 != "cm" && y1 != "m" && y1 !="in" && y1 != "ft"){
-			std::cout<<"bad y1\n";
-			return 0;
-		} 
-		if(x1 != "cm" && x1 != "m" && x1 !="in" && x1 != "ft"){
-			std::cout<<"bad x1\n";
-			return 0;
-		} 
 		double meters_temp=convert_temp(y1,temp);
 		double meters_x=convert_temp(x1,x);
+		if(meters_x == -1 || meters_temp==-1){
+			std::cout<<"bad unit type\n";
+			return 0;
+		}
 		v.push_back(meters_temp); 
-		v.push_back(meters_x);	
+		v.push_back(meters_x);
 		if(x==temp){
 			std::cout<<"The numbers are equal.\n";
 		} else if  ((x-temp <1.0/100 && x>temp) || (temp-x < 1.0/100&& temp>x)){
@@ -64,10 +65,10 @@ int main(){
 		} 
 		if(meters_x>meters_temp){
 			std::cout<<"The smaller value is the first one " <<temp << y1<<" or "<<meters_temp<<" meters\n";
-			std::cout<<"The larger value is: the second one " <<x<<x1<<" or "<<meters_x<<"meters \n";
+			std::cout<<"The larger value is: the second one " <<x<<x1<<" or "<<meters_x<<" meters \n";
 		} else if (meters_x<meters_temp){
 			std::cout<<"The smaller value is the second one: "<<x<< x1 <<" or "<<meters_x<< " meters\n";
-			std::cout<<"The larger value is the first one: " <<temp<<y1<<" or "<<meters_temp<<"meters \n";
+			std::cout<<"The larger value is the first one: " <<temp<<y1<<" or "<<meters_temp<<" meters \n";
 		} else if(meters_x==meters_temp){
 			std::cout<<"The numbers are equal.\n";
 		}
